fabd36122bc30cec2101d8f35c6b05c4589d3c24
tim a askew <askew_design@hotmail.com>
2013-06-23 15:03:19 -0500

    Testing

---
+++ LearningOOP1/LearningOOP1/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "addition.h"
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+	Addition addup;
+	
+	Addition first(2,3);
+
+
+
+	system("pause");
+	return 0;
+}
---
+++ LearningOOP1/LearningOOP1/addition.h
@@ -0,0 +1,22 @@
+#ifndef _ADDITION
+#define _ADDITION
+#include <iostream>
+
+
+using namespace std;
+
+class Addition
+{
+
+public:
+	Addition();
+	
+	Addition(int num1, int num2);
+	
+
+private:
+	int num1;
+	int num2;
+};
+
+#endif
